9d5e74f8aaa2e7ae1e4415ab7fb8543897caacfe
Fletcher Tomalty <fletcher@nomulous.com>
2010-07-18 22:50:05 -0400

    removed Snow Leopard depency by deleting the NSApplicationDelegate protocol

--- Cwm_FjordAppDelegate.h
+++ Cwm_FjordAppDelegate.h
@@ -8,7 +8,7 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface Cwm_FjordAppDelegate : NSObject <NSApplicationDelegate> {
+@interface Cwm_FjordAppDelegate : NSObject {
 	NSString *string;
 	NSString *processedString;
 	NSMutableCharacterSet *absentChars;
